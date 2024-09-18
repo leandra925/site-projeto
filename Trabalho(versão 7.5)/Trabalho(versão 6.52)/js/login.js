54f9aeb55593c587d53a54633c3546a72c7e73b4
leandra925 <168788544+leandra925@users.noreply.github.com>
2024-09-18 11:27:12 -0300

    Add files via upload

---
+++ Trabalho(versão 7.5)/Trabalho(versão 6.52)/js/login.js
@@ -0,0 +1,14 @@
+/*function Login() {
+    const usuario = document.getElementById('loginUsuario').value;
+    const senha = document.getElementById('loginSenha').value;
+
+    const usuarioCadastrado = localStorage.getItem('usuario');
+    const senhaCadastrada = localStorage.getItem('senha');
+
+    if (usuario === usuarioCadastrado && senha === senhaCadastrada) {
+        alert("Login bem-sucedido!");
+        window.location.href = 'index.html'; 
+    } else {
+        alert("Usuário ou senha incorretos!");
+    }
+}*/
